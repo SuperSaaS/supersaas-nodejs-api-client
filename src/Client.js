b7a0221d86c9925ad745a2481ae32f820126e3e5
nikhilg <nikhil@supersaas.com>
2019-07-04 14:09:10 +0200

    updating api-client version to 1.1.0

--- src/Client.js
+++ src/Client.js
@@ -25,7 +25,7 @@
     this.users = new Users(this);
   }
   Client.API_VERSION = '1';
-  Client.VERSION = '1.0.2';
+  Client.VERSION = '1.1.0';
 
   Client.prototype.get = function(path, query, callback) {
     return this.request('GET', path, null, query, callback);
